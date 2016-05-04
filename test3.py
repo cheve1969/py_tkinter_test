4c2a65bd2f31650808804035ee4f029a4ab26f6b
Cheve Echeverria <cheve1969@gmail.com>
2016-05-04 13:47:08 -0300

    Lista

--- test3.py
+++ test3.py
@@ -1,38 +1,24 @@
-try:
-  import Tkinter              # Python 2
-  import ttk
-except ImportError:
-  import tkinter as Tkinter   # Python 3
-  import tkinter.ttk as ttk
-
-
-def main():
-
-  root = Tkinter.Tk()
-
-  ft = ttk.Frame()
-  fb = ttk.Frame()
-
-  ft.pack(expand=True, fill=Tkinter.BOTH, side=Tkinter.TOP)
-  fb.pack(expand=True, fill=Tkinter.BOTH, side=Tkinter.TOP)
-
-  pb_hd = ttk.Progressbar(ft, orient='horizontal', mode='determinate')
-  pb_hD = ttk.Progressbar(ft, orient='horizontal', mode='indeterminate')
-  pb_vd = ttk.Progressbar(fb, orient='vertical', mode='determinate')
-  pb_vD = ttk.Progressbar(fb, orient='vertical', mode='indeterminate')
-
-  pb_hd.pack(expand=True, fill=Tkinter.BOTH, side=Tkinter.TOP)
-  pb_hD.pack(expand=True, fill=Tkinter.BOTH, side=Tkinter.TOP)
-  pb_vd.pack(expand=True, fill=Tkinter.BOTH, side=Tkinter.LEFT)
-  pb_vD.pack(expand=True, fill=Tkinter.BOTH, side=Tkinter.LEFT)
-
-  pb_hd.start(50)
-  pb_hD.start(50)
-  pb_vd.start(50)
-  pb_vD.start(50)
-
-  root.mainloop()
-
-
-if __name__ == '__main__':
-  main()
+from tkinter import *
+from tkinter import ttk
+ 
+root = Tk()
+ 
+tree = ttk.Treeview(root)
+ 
+tree["columns"]=("one","two")
+tree.column("one", width=100 )
+tree.column("two", width=100)
+tree.heading("one", text="coulmn A")
+tree.heading("two", text="column B")
+ 
+tree.insert("" , 0,    text="Line 1", values=("1A","1b"))
+ 
+id2 = tree.insert("", 1, "dir2", text="Dir 2")
+tree.insert(id2, "end", "dir 2", text="sub dir 2", values=("2A","2B"))
+ 
+##alternatively:
+tree.insert("", 3, "dir3", text="Dir 3")
+tree.insert("dir3", 3, text=" sub dir 3",values=("3A"," 3B"))
+ 
+tree.pack()
+root.mainloop()
